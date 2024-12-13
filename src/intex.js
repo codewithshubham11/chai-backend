831a1a37eda451f8bef8ee58ad34b8583c42a716
Shubham Yadav <shubh.yadav11032000@gmail.com>
2024-12-13 14:18:38 +0530

    connet to mongo atlas

--- src/intex.js
+++ src/intex.js
@@ -1,3 +1,4 @@
+import app  from "./app.js";
 import connectDB from "./db/intex.js";
 import dotenv from "dotenv";
 
@@ -6,8 +7,17 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server has been connect at port: ${process.env.PORT}`)
+    })
+    
+})
 
 
+.catch((err)=>{
+    console.log("MONGODB connection failed!!!",err)
+})
 
 
 
