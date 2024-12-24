bf59fed3ca5a2ec4b9450ca8122c52cc6179f4d7
Shubham Yadav <shubh.yadav11032000@gmail.com>
2024-12-24 08:31:14 +0530

    Fix import statement for jsonwebtoken in user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,8 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uoploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import jwt from JsonWebToken;
-
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefreshToken = async(userId)=>{
     try {
